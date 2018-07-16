d2dca1a585d2dc85b570ea1715455fb6f43ea514
Gabriel <gabriel.altay@gmail.com>
2018-07-15 21:15:30 -0400

    ubuntu sym links for gcc 6

---
+++ gcc_sym_links/set_gcc_6.sh
@@ -0,0 +1,18 @@
+sudo rm -f /usr/bin/g++
+sudo ln -s /usr/bin/g++-6 /usr/bin/g++
+
+
+sudo rm -f /usr/bin/gcc
+sudo ln -s /usr/bin/gcc-6 /usr/bin/gcc
+
+
+sudo rm -f /usr/bin/gcc-ar
+sudo ln -s /usr/bin/gcc-ar-6 /usr/bin/gcc-ar
+
+
+sudo rm -f /usr/bin/gcc-nm
+sudo ln -s /usr/bin/gcc-nm-6 /usr/bin/gcc-nm
+
+
+sudo rm -f /usr/bin/gcc-ranlib
+sudo ln -s /usr/bin/gcc-ranlib-6 /usr/bin/gcc-ranlib
